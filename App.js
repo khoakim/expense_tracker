8d2abff0f5f7848868e30e47c308cfd960b0833e
khoakim <khoakim@atosp.onmicrosoft.com>
2021-11-07 22:15:47 +1100

    checked

--- App.js
+++ App.js
@@ -3,8 +3,7 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { Home } from "./screens/";
-import AppLoading from "expo-app-loading";
-import { useFonts } from "expo-font";
+
 
 const theme = {
   ...DefaultTheme,
@@ -17,30 +16,7 @@ const theme = {
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-  let [fontsLoaded] = useFonts({
-    "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
-    "Roboto-BlackItalic": require("./assets/fonts/Roboto-BlackItalic.ttf"),
-    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
-    "Roboto-BoldItalic": require("./assets/fonts/Roboto-BoldItalic.ttf"),
-    "Roboto-Italic": require("./assets/fonts/Roboto-Italic.ttf"),
-    "Roboto-Light": require("./assets/fonts/Roboto-Light.ttf"),
-    "Roboto-LightItalic": require("./assets/fonts/Roboto-LightItalic.ttf"),
-    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
-    "Roboto-MediumItalic": require("./assets/fonts/Roboto-MediumItalic.ttf"),
-    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
-    "Roboto-Thin": require("./assets/fonts/Roboto-Thin.ttf"),
-    "Roboto-ThinItalic": require("./assets/fonts/Roboto-ThinItalic.ttf"),
-    "RobotoCondensed-Bold": require("./assets/fonts/RobotoCondensed-Bold.ttf"),
-    "RobotoCondensed-BoldItalic": require("./assets/fonts/RobotoCondensed-BoldItalic.ttf"),
-    "RobotoCondensed-Italic": require("./assets/fonts/RobotoCondensed-Italic.ttf"),
-    "RobotoCondensed-Light": require("./assets/fonts/RobotoCondensed-Light.ttf"),
-    "RobotoCondensed-LightItalic": require("./assets/fonts/RobotoCondensed-LightItalic.ttf"),
-    "RobotoCondensed-Regular": require("./assets/fonts/RobotoCondensed-Regular.ttf"),
-  });
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  } else {
     return (
       <NavigationContainer theme={theme}>
         <Stack.Navigator
@@ -53,7 +29,7 @@ const App = () => {
         </Stack.Navigator>
       </NavigationContainer>
     );
-  }
+  
 };
 
 export default App;
