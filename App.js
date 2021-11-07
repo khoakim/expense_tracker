ff162bc6a5543230c650c76cff4b4d95e11a9d4d
khoakim <khoakim@atosp.onmicrosoft.com>
2021-11-07 21:50:41 +1100

    Saved

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { Home } from "./screens/";
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    border: "transparent",
   },
-});
+};
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  let [fontsLoaded] = useFonts({
+    "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
+    "Roboto-BlackItalic": require("./assets/fonts/Roboto-BlackItalic.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-BoldItalic": require("./assets/fonts/Roboto-BoldItalic.ttf"),
+    "Roboto-Italic": require("./assets/fonts/Roboto-Italic.ttf"),
+    "Roboto-Light": require("./assets/fonts/Roboto-Light.ttf"),
+    "Roboto-LightItalic": require("./assets/fonts/Roboto-LightItalic.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-MediumItalic": require("./assets/fonts/Roboto-MediumItalic.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Thin": require("./assets/fonts/Roboto-Thin.ttf"),
+    "Roboto-ThinItalic": require("./assets/fonts/Roboto-ThinItalic.ttf"),
+    "RobotoCondensed-Bold": require("./assets/fonts/RobotoCondensed-Bold.ttf"),
+    "RobotoCondensed-BoldItalic": require("./assets/fonts/RobotoCondensed-BoldItalic.ttf"),
+    "RobotoCondensed-Italic": require("./assets/fonts/RobotoCondensed-Italic.ttf"),
+    "RobotoCondensed-Light": require("./assets/fonts/RobotoCondensed-Light.ttf"),
+    "RobotoCondensed-LightItalic": require("./assets/fonts/RobotoCondensed-LightItalic.ttf"),
+    "RobotoCondensed-Regular": require("./assets/fonts/RobotoCondensed-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <NavigationContainer theme={theme}>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+          initialRouteName={"Home"}
+        >
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+};
+
+export default App;
